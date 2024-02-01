1fee2342c4e3aa54b6fd539ef6bfc28fe9403596
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-01 20:37:00 +0200

    .

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * @parent: Pointer to the parent node
  * @value: Value to put in the new node
  * Return: Pointer to the new node
-*/
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
