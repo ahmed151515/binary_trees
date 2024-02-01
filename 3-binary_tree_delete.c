47f3858836083d27a2d686fee3bc0c5ca4bffd8f
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-01 21:09:25 +0200

    .

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ * Return: Nothing
+*/
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
