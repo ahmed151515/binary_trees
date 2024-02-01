3410cac62680b974336af34925e7abe39a3d1a46
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-01 20:54:02 +0200

    .

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		new->left = parent->left;
+		parent->left->parent = new;
 		parent->left = new;
 	}
 
--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ CC = gcc
 CFLAGS = -Wall -Werror -Wextra -pedantic
 
 all: run
-
+	@./run
 run: *.c
-	$(CC) $(CFLAGS) *.c -o run
+	@$(CC) $(CFLAGS) *.c -o run
 
 clean:
 	rm -f run
